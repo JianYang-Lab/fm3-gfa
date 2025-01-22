8959f019ea03e728741dba597c4555d91b359ca3
wjwei-handsome <weiwenjie@westlake.edu.cn>
2025-01-23 00:29:13 +0800

    pipeline test

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,40 @@
-use anyhow::Result;
-use fm3_gfa::echart::EchartGraph;
-use fm3_gfa::gml::{GMLObject, Graph};
-use fm3_gfa::Layout;
 use std::str::FromStr;
 
+use anyhow::Result;
+// use fm3_gfa::echart::EchartGraph;
+// use fm3_gfa::gml::{GMLObject, Graph};
+use fm3_gfa::layout::Layout;
+use fm3_gfa::{
+    bfs::extract_subgraph_by_bfs,
+    echart::EchartGraph,
+    gfa::gfa_to_graph,
+    gml::{GMLGraph, GMLObject},
+    vcf::parse_vcf_file,
+};
+use indicatif::ProgressBar;
+
+// use fm3_gfa::Layout;
+// use std::str::FromStr;
+
 fn main() -> Result<()> {
-    // stdin -> GML -> Graph
-    let gml_content = std::fs::read_to_string("test.gml").unwrap();
-    let layout = Layout::new()?;
-    let res = layout.run(&gml_content)?;
-    let layout_graph = GMLObject::from_str(&res)?;
-    let layout_graph = Graph::from_gml(layout_graph)?;
-    // println!("{:?}", layout_graph);
-    // println!("{}", res);
-    let echart_graph = EchartGraph::from_gml(layout_graph)?;
-    eprintln!("echart_graph done");
+    // GFA/VCF => Sub-GFA => GML => Layout(GML) => Echart
 
-    // 5. 保存结果
-    let oneline = echart_graph.oneline_stdout()?;
-    eprintln!("oneline done");
+    let g = gfa_to_graph("tests/test.gfa")?;
 
-    // 6. stdout
-    println!("{}", oneline);
-
-    // let gml = GMLObject::from_str(&gml_content)?;
-    // let graph = Graph::from_gml(gml)?;
-    // println!("{:?}", graph);
-    // // layout(&mut graph)?;
-    // let mut g = graph.to_petgraph();
-    // // add node attr in g
-    // // 2. 配置布局选项
-    // let options = LayoutOptions::new()
-    //     .with_force_model(ForceModel::New)
-    //     .with_edge_length(100.0)
-    //     .with_iterations(100);
-
-    // // 3. 创建并运行布局
-    // let mut layout = FMMMLayout::new().with_options(options);
-    // layout.run(&mut g)?;
-    // // println!("{:?}", g);
-    // let echart_graph = EchartGraph::from_layout_graph(&g);
-    // let oneline = echart_graph.oneline_stdout()?;
-    // std::fs::write("vis/test.json", oneline.as_bytes())?;
-
-    // multilevel_layout(&mut graph, &config);
+    let bubbles = parse_vcf_file("tests/tmp.vcf")?;
+    let bar = ProgressBar::new(bubbles.len() as u64);
+    for bubble in bubbles {
+        let sub_graph = extract_subgraph_by_bfs(&bubble, &g)?;
+        let gml_content = sub_graph.to_gml_string();
+        let layout = Layout::new()?;
+        let res = layout.run(&gml_content)?;
+        let layout_graph = GMLObject::from_str(&res)?;
+        let layout_graph = GMLGraph::from_gml(layout_graph)?;
+        let echart_graph = EchartGraph::from_gml(layout_graph)?;
+        let oneline = echart_graph.oneline_stdout()?;
+        println!("{}", oneline);
+        bar.inc(1);
+    }
 
     Ok(())
 }
